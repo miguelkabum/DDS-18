d6283df083ada470d7571f3ba9db5b3257ae1a0c
Miguel Pereira <miguel.p.oliveira7@aluno.senai.br>
2024-08-05 17:26:21 -0300

    versão 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ function App() {
   return (
     <>
       <div className="cards">
-        <Card />
-        <Card />
+        <Card title="Marrom bom bom" text="Não deixe o samba morrer..."/>
+        <Card title="Alcione" text="Sou loba..."/>
+        <Card title="Lenda" text="Mas tem que me prender..."/>
+        <Card title="A maior do samba" text="A minha lei você tem que saber..."/>
       </div>
       
     </>
--- src/componets/card/index.jsx
+++ src/componets/card/index.jsx
@@ -1,13 +1,13 @@
 import styles from "./style.module.css"
-export default function Card(){
+export default function Card(props){
     return (
 
         <div className={styles.container}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBzj6P1ccLIW1pHyPWIWpL_nwgS--86xF-RQ&s" alt="" />
-            <div>
-                <h2>Marrom bombom</h2>
-                <p>Não deixe o samba morrer...</p>
-                <button>Não deixar o samba morrer!</button>
+            <img className={styles.img} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBzj6P1ccLIW1pHyPWIWpL_nwgS--86xF-RQ&s" alt="" />
+            <div className={styles.text}>
+                <h2>{props.title}</h2>
+                <p>{props.text}</p>
+                <button className={styles.button}>Não deixar o samba morrer!</button>
             </div>
         </div>
 
